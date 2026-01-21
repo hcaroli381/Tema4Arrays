8dc7cc0011dcdfde9917ef27c224722119de6495
hcaroli381 <hcaroli381@g.educaand.es>
2026-01-21 15:36:43 +0100

    Add files via upload

--- Tema4Arrays/src/ejercicios4_3ClaseArrays/Ejercicio1.java
+++ Tema4Arrays/src/ejercicios4_3ClaseArrays/Ejercicio1.java
@@ -1,1 +1,25 @@
+package ejercicios4_3ClaseArrays;
 
+import java.util.Arrays;
+
+public class Ejercicio1 {
+	int matriz[][] = { { 1, 2, 3 }, { 4, 5 }, { 6, 7, 8, 9 } };
+
+	public void show() {
+		int matrizShow[][] = doblarColumnas(matriz);
+		System.out.printf("%s", Arrays.deepToString(matrizShow));
+	}
+
+	public int[][] doblarColumnas(int[][] matriz) {
+		for (int i = 0; i < matriz.length; i++) {
+			matriz[i] = Arrays.copyOf(matriz[i], matriz[i].length * 2);
+		}
+
+		return matriz;
+	}
+
+	public static void main(String[] args) {
+		new Ejercicio1().show();
+	}
+
+}
